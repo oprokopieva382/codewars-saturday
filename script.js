4873813afa103d91bf7d062e94a2e0701ef001e1
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 15:50:57 -0600

    longestConsec

--- script.js
+++ script.js
@@ -113,3 +113,21 @@ function duplicateCount(text) {
       .match(/([^])\1+/g) || []
   ).length;
 }
+//
+function longestConsec(strarr, k) {
+  let arrLength = strarr.length;
+  let arr = [];
+
+  if (arrLength == 0 || k > arrLength || k <= 0) return "";
+
+  for (let i = 0; i <= arrLength - k; i++) {
+    let current = strarr[i];
+    for (let ii = k, jj = 1; ii > 1; ii--, jj++) {
+      current += strarr[i + jj];
+    }
+    arr.push(current);
+  }
+  return arr.reduce(function (a, b) {
+    return a.length > b.length ? a : a.length == b.length ? a : b;
+  });
+}
