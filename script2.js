6d71e51b341505393c38a7e4ce388411bf0fae3d
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 17:11:57 -0600

    findOdd

--- script2.js
+++ script2.js
@@ -44,3 +44,15 @@ const invert = (array) => array.map((num) => -num);
 String.prototype.toJadenCase = function () {
   return this.replace(/(?:^|\s)\S/g, (word) => word.toUpperCase());
 };
+//
+function findOdd(A) {
+  let obj = {};
+  for (let i = 0; i < A.length; i++) {
+    obj[A[i]] = (obj[A[i]] || 0) + 1;
+  }
+  for (let key in obj) {
+    if (obj[key] % 2 !== 0) {
+      return Number(key);
+    }
+  }
+}
