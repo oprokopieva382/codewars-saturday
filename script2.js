ea17b64fd609ad1b2a54b8f378df4904f9c8ee0c
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 16:37:51 -0600

    arithmetic

--- script2.js
+++ script2.js
@@ -5,3 +5,20 @@ function repeatStr(n, s) {
 function check(a, x) {
   return a.includes(x);
 }
+//
+function arithmetic(a, b, operator) {
+  switch (operator) {
+    case "add":
+      return a + b;
+      break;
+    case "subtract":
+      return a - b;
+      break;
+    case "multiply":
+      return a * b;
+      break;
+    case "divide":
+      return a / b;
+      break;
+  }
+}
