2aac802f66a117970aa48a6411f464a179afab51
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 15:53:03 -0600

    shortest version longestConsec

--- script.js
+++ script.js
@@ -114,20 +114,33 @@ function duplicateCount(text) {
   ).length;
 }
 //
+// function longestConsec(strarr, k) {
+//   let arrLength = strarr.length;
+//   let arr = [];
+
+//   if (arrLength == 0 || k > arrLength || k <= 0) return "";
+
+//   for (let i = 0; i <= arrLength - k; i++) {
+//     let current = strarr[i];
+//     for (let ii = k, jj = 1; ii > 1; ii--, jj++) {
+//       current += strarr[i + jj];
+//     }
+//     arr.push(current);
+//   }
+//   return arr.reduce(function (a, b) {
+//     return a.length > b.length ? a : a.length == b.length ? a : b;
+//   });
+// }
+
+//shortest version
 function longestConsec(strarr, k) {
-  let arrLength = strarr.length;
-  let arr = [];
-
-  if (arrLength == 0 || k > arrLength || k <= 0) return "";
-
-  for (let i = 0; i <= arrLength - k; i++) {
-    let current = strarr[i];
-    for (let ii = k, jj = 1; ii > 1; ii--, jj++) {
-      current += strarr[i + jj];
+  var longest = "";
+  for (var i = 0; k > 0 && i <= strarr.length - k; i++) {
+    var tempArray = strarr.slice(i, i + k);
+    var tempStr = tempArray.join("");
+    if (tempStr.length > longest.length) {
+      longest = tempStr;
     }
-    arr.push(current);
   }
-  return arr.reduce(function (a, b) {
-    return a.length > b.length ? a : a.length == b.length ? a : b;
-  });
+  return longest;
 }
