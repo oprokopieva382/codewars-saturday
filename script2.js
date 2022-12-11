06203aa17663d5c697047cbacf159346774a2c4b
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 22:35:11 -0600

    Maximum subarray sum

--- script2.js
+++ script2.js
@@ -122,3 +122,16 @@ const rot13 = (message) => {
     (letter) => cipher[originalAlpha.indexOf(letter)]
   );
 };
+//
+var maxSequence = function (arr) {
+  var min = 0,
+    ans = 0,
+    i,
+    sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+};
