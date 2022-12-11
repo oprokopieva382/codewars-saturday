4b928d7d0cab7b5bdaff1c2124e2d8d137433f12
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-10 22:07:06 -0600

    getDivisorsCnt

--- script2.js
+++ script2.js
@@ -84,3 +84,13 @@ function wave(str) {
   }
   return waveArr;
 }
+//
+function getDivisorsCnt(n) {
+  let output = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      output += 1;
+    }
+  }
+  return output;
+}
